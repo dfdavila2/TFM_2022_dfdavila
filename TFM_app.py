ffe37b7652fdca278c946515601df71b69e2bba7
dfdavila <57158584+dfdavila2@users.noreply.github.com>
2022-06-02 20:38:52 -0500

    Update TFM_app.py

--- TFM_app.py
+++ TFM_app.py
@@ -5,8 +5,6 @@ from streamlit_pandas_profiling import st_profile_report
 
 st.header('`streamlit_pandas_profiling`')
 
-# df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-
 df = pd.read_csv('https://raw.githubusercontent.com/dfdavila2/TFM_2022_dfdavila/main/cleaned_data.csv?token=GHSAT0AAAAAABQBEW4C5HXFH4ZLMQRPFSESYUO6KHQ')
 
 # https://github.com/dfdavila2/TFM_2022_dfdavila/blob/main/cleaned_data.csv')
