49042ab207699879a889d639253fdb8a1d116a2c
dfdavila <57158584+dfdavila2@users.noreply.github.com>
2022-06-15 21:22:06 -0500

    Update EDA_app.py

--- EDA_app.py
+++ EDA_app.py
@@ -3,10 +3,9 @@ import pandas as pd
 import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
-@st.cache  
-
 st.header('`Análisis exploratorio de los datos`')
 
+@st.cache  
 df = pd.read_csv('https://raw.githubusercontent.com/dfdavila2/TFM_2022_dfdavila/main/cleaned_data.csv?token=GHSAT0AAAAAABQBEW4C5HXFH4ZLMQRPFSESYUO6KHQ')
 
 # https://github.com/dfdavila2/TFM_2022_dfdavila/blob/main/cleaned_data.csv')
